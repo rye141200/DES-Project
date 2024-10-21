c36ab3068551f78f238cf8b867fd23f84ca2154a
rye141200 <126208685+rye141200@users.noreply.github.com>
2024-10-21 06:00:40 +0300

    Expansion, XORs and post SBox permutation

--- Round.cpp
+++ Round.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 using namespace std;
 #define MASK48BITS(v) {v & 0xFFFFFFFFFFFF};
-#define EXTRACTBIT(input,bitPosition) {(input>>bitPosition)&1}
+#define EXTRACTBIT(input,bitPosition) {(input>>bitPosition) & 1}
 void printHex(uint64_t input){
     std::cout << "Hexadecimal: " << "0x" << std::hex << input << std::endl;
 }
@@ -19,7 +19,66 @@ static char expansionTable[] = {
     28, 29, 30, 31, 32,  1
 };
 
+static char postSBoxPermutation[] = {
+    16,  7, 20, 21, 
+    29, 12, 28, 17, 
+     1, 15, 23, 26, 
+     5, 18, 31, 10, 
+     2,  8, 24, 14, 
+    32, 27,  3,  9, 
+    19, 13, 30,  6, 
+    22, 11,  4, 25
+};
 
+static char S[8][64] = {{
+    /* S1 */
+    14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7,  
+     0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8,  
+     4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0, 
+    15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13
+},{
+    /* S2 */
+    15,  1,  8, 14,  6, 11,  3,  4,  9,  7,  2, 13, 12,  0,  5, 10,  
+     3, 13,  4,  7, 15,  2,  8, 14, 12,  0,  1, 10,  6,  9, 11,  5,  
+     0, 14,  7, 11, 10,  4, 13,  1,  5,  8, 12,  6,  9,  3,  2, 15, 
+    13,  8, 10,  1,  3, 15,  4,  2, 11,  6,  7, 12,  0,  5, 14,  9
+},{
+    /* S3 */
+    10,  0,  9, 14,  6,  3, 15,  5,  1, 13, 12,  7, 11,  4,  2,  8,  
+    13,  7,  0,  9,  3,  4,  6, 10,  2,  8,  5, 14, 12, 11, 15,  1,  
+    13,  6,  4,  9,  8, 15,  3,  0, 11,  1,  2, 12,  5, 10, 14,  7,
+     1, 10, 13,  0,  6,  9,  8,  7,  4, 15, 14,  3, 11,  5,  2, 12
+},{
+    /* S4 */
+     7, 13, 14,  3,  0,  6,  9, 10,  1,  2,  8,  5, 11, 12,  4, 15,  
+    13,  8, 11,  5,  6, 15,  0,  3,  4,  7,  2, 12,  1, 10, 14,  9,  
+    10,  6,  9,  0, 12, 11,  7, 13, 15,  1,  3, 14,  5,  2,  8,  4,
+     3, 15,  0,  6, 10,  1, 13,  8,  9,  4,  5, 11, 12,  7,  2, 14
+},{
+    /* S5 */
+     2, 12,  4,  1,  7, 10, 11,  6,  8,  5,  3, 15, 13,  0, 14,  9, 
+    14, 11,  2, 12,  4,  7, 13,  1,  5,  0, 15, 10,  3,  9,  8,  6, 
+     4,  2,  1, 11, 10, 13,  7,  8, 15,  9, 12,  5,  6,  3,  0, 14, 
+    11,  8, 12,  7,  1, 14,  2, 13,  6, 15,  0,  9, 10,  4,  5,  3
+},{
+    /* S6 */
+    12,  1, 10, 15,  9,  2,  6,  8,  0, 13,  3,  4, 14,  7,  5, 11,
+    10, 15,  4,  2,  7, 12,  9,  5,  6,  1, 13, 14,  0, 11,  3,  8,
+     9, 14, 15,  5,  2,  8, 12,  3,  7,  0,  4, 10,  1, 13, 11,  6,
+     4,  3,  2, 12,  9,  5, 15, 10, 11, 14,  1,  7,  6,  0,  8, 13
+},{
+    /* S7 */
+     4, 11,  2, 14, 15,  0,  8, 13,  3, 12,  9,  7,  5, 10,  6,  1,
+    13,  0, 11,  7,  4,  9,  1, 10, 14,  3,  5, 12,  2, 15,  8,  6,
+     1,  4, 11, 13, 12,  3,  7, 14, 10, 15,  6,  8,  0,  5,  9,  2,
+     6, 11, 13,  8,  1,  4, 10,  7,  9,  5,  0, 15, 14,  2,  3, 12
+},{
+    /* S8 */
+    13,  2,  8,  4,  6, 15, 11,  1, 10,  9,  3, 14,  5,  0, 12,  7,
+     1, 15, 13,  8, 10,  3,  7,  4, 12,  5,  6, 11,  0, 14,  9,  2,
+     7, 11,  4,  1,  9, 12, 14,  2,  0,  6, 10, 13, 15,  3,  5,  8,
+     2,  1, 14,  7,  4, 10,  8, 13, 15, 12,  9,  0,  3,  5,  6, 11
+}};
 //! Type declarations
 struct Unsigned48Bits{
     uint64_t value: 48;
@@ -46,16 +105,50 @@ uint32_t permutation(uint32_t SBoxed);
 uint32_t XOR32Bits(uint32_t permuted,uint32_t LiPrev);
 
 int main(){
-    // Optionally, print with "0x" prefix
-    printHex(expansionPermutation(0x53214011).value);
+    
+    // printHex(expansionPermutation(0x2EBA0635).value);
+    //printHex(XOR48Bits(Unsigned48Bits(0x95d5f400c1aa),Unsigned48Bits(0xB02679B49A5)).get());
+    // printHex(permutation(0x2487B114));
+    //printHex(XOR32Bits(0xA34033B0,0x1CCD0EA8));
+    uint64_t S1 = (0x1200FF00FF00 & 0xFC0000000000)>>42;
+    
+    uint64_t S2 = (0x1200FF00FF00 & 0x03F000000000) >>36;
+    printHex(S2);
 }
 
-Unsigned48Bits expansionPermutation(uint32_t RiPrev){
+Unsigned48Bits expansionPermutation(uint32_t RiPrev){   
     Unsigned48Bits expanded;
     for(int i =0;i<48;i++){
         int bitPosition = expansionTable[i]-1; //! for 0 based indexing
-        uint64_t bit = EXTRACTBIT(RiPrev,bitPosition);
+        uint64_t bit = EXTRACTBIT(RiPrev,31-bitPosition); //! 31 because C++ treats hexadecimals as little endian
         expanded.set(expanded.get() | (bit<<(47-i)));
     }
     return expanded;
+}
+Unsigned48Bits XOR48Bits(Unsigned48Bits expandedPermuted,Unsigned48Bits Ki){
+    return expandedPermuted.get() ^ Ki.get();    
+}
+
+uint32_t SBox(Unsigned48Bits XORedExpandedPermutedWithKey){
+    //TODO
+    uint64_t inputValue = XORedExpandedPermutedWithKey.get();
+    //!1) Partition the input into 8 partitions
+    uint64_t S1 = (inputValue & 0xFC0000000000) >>42;
+    uint64_t S2 = (inputValue & 0x03F000000000) >>36;
+    
+    //!2) Handle the little endian representation by subtracting the bit position from 47, when extracting the bit
+}
+
+uint32_t permutation(uint32_t SBoxed){
+    uint32_t permuted;
+    for(int i =0;i<32;i++){
+        int bitPosition = postSBoxPermutation[i]-1; //! 0 based indexing
+        uint32_t bit = EXTRACTBIT(SBoxed,31-bitPosition);
+        permuted |= bit <<(31-i);
+    }
+    return permuted;
+}
+
+uint32_t XOR32Bits(uint32_t permuted,uint32_t LiPrev){
+    return permuted ^ LiPrev;
 }
